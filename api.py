f9535303392a6be8c59f3c085536084059c918ef
Laura Joy Erb <laurajoy@bu.edu>
2020-02-02 15:52:58 -0500

    added flask and routing

--- api.py
+++ api.py
@@ -3,13 +3,33 @@
 # EC500: Building Software
 # Project 2: API Design
 
-# import required modules
+import flask
 import requests
 import json
+import jsonify
 from config import api_key
 
-def print_weather(response):
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
 
+def print_weather(weather):
+    print("Weather for " + weather["city_name"] + ":")
+    print("\tTemperature: ", end="")
+    print(round(weather["temp"], 2), end="")
+    print("ºC")
+    print("\tFeels Like: ", end="")
+    print(round(weather["feels_like"], 2), end="")
+    print("ºC")
+    print("\tPressure: ", end="")
+    print(weather["pressure"])
+    print("\tHumidity: ", end="")
+    print(weather["humidity"], end="")
+    print("%")
+    print("\tCurrent weather status: " + weather["weather_desc"])
+    print("\tWind Speed: ", end="")
+    print(weather["wind_speed"])
+
+def parse_weather(response):
     city_name = response["name"]
     feels_like = response["main"]["feels_like"]
     temperature = response["main"]["temp"]
@@ -18,21 +38,16 @@ def print_weather(response):
     weather_description = response["weather"][0]["description"]
     wind_speed = response["wind"]["speed"]
 
-    print("Weather for " + city_name + ":")
-    print("\tTemperature: ", end="")
-    print(round(temperature - 273.15, 2), end="")
-    print("ºC")
-    print("\tFeels Like: ", end="")
-    print(round(feels_like - 273.15, 2), end="")
-    print("ºC")
-    print("\tPressure: ", end="")
-    print(pressure)
-    print("\tHumidity: ", end="")
-    print(humidity, end="")
-    print("%")
-    print("\tCurrent weather status: " + weather_description)
-    print("\tWind Speed: ", end="")
-    print(wind_speed)
+    weather = {
+        'city_name' : city_name,
+        'feels_like' : round(feels_like - 273.15,2),
+        'temp' : round(temperature - 273.15,2),
+        'pressure' : pressure,
+        'humidity' : humidity,
+        'weather_desc' : weather_description,
+        'wind_speed' : wind_speed
+    }
+    return weather
 
 def get_weather(city):
     base_url = "http://api.openweathermap.org/data/2.5/weather?"
@@ -44,11 +59,22 @@ def get_weather(city):
 
     if response["cod"] != "404":
 
-        print_weather(response)
-        return 0
+        weather_dict = parse_weather(response)
+        print_weather(weather_dict)
+        return weather_dict
 
     else:
         print("Error: Could not find city")
         return -1
 
-get_weather("Boston")
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Weather API</h1><p>by Laura Joy Erb</p>"
+
+@app.route('/api/weather', methods=['GET'])
+def api():
+    weath = get_weather("Boston")
+    return weath
+
+app.run()
