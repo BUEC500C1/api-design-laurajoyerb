d65d231fd4fcbb75887e65259c7f109146d9a99e
Laura Joy Erb <laurajoy@bu.edu>
2020-02-04 10:27:02 -0500

    updated test file

--- test.py
+++ test.py
@@ -1,10 +1,33 @@
+from api import get_city_from_ident
+from api import get_city_from_name
 from api import get_weather
 
-def test1():
-    assert get_weather("Boston") == 0
+def test_ident1():
+    assert get_city_from_ident("00AK") == "Anchor Point"
 
-def test2():
-    assert get_weather("London") == 0
+def test_ident2():
+    assert get_city_from_ident("PABV") == "Birchwood"
 
-def test3():
-    assert get_weather("Blah Blah") == -1
+def test_ident3():
+    assert get_city_from_ident("PAVD") == "Valdez"
+
+def test_invalid_ident():
+    assert get_city_from_ident("HEYY") == ""
+
+def test_name1():
+    assert get_city_from_name("Talkeetna Airport") == "Talkeetna"
+
+def test_name2():
+    assert get_city_from_name("Gulkana Airport") == "Gulkana"
+
+def test_name3():
+    assert get_city_from_name("Merrill Field") == "Anchorage"
+
+def test_invalid_name():
+    assert get_city_from_name("heyyy") == ""
+
+def test_invalid1():
+    assert get_weather("234") == 0
+
+def test_invalid2():
+    assert get_weather("") == 0
